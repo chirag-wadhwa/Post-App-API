b4ec9666d17fc43c916901948594c6e3391daa18
Chirag Wadhwa <chiragwadhwa.55555@gmail.com>
2020-05-23 08:32:46 +0530

    socket.io

--- Back-end/app.js
+++ Back-end/app.js
@@ -61,6 +61,10 @@ mongoose
     'mongodb+srv://Chirag:krishna123@clustermain-r7co8.mongodb.net/messages?retryWrites=true&w=majority', { useUnifiedTopology: true, useNewUrlParser: true }
   )
   .then(result => {
-    app.listen(8080);
+    const server = app.listen(8080);
+    const io = require('./socket').init(server);
+    io.on('connection', socket => {
+      console.log('Client connected');
+    });
   })
   .catch(err => console.log(err));
